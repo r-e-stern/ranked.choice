9ba3b7fac234283bbb5e28e88b2b0289dcf6b993
r-e-stern <rara.rezra@gmail.com>
2019-03-13 13:10:27 -0700

    Initial commit

---
+++ rcv.js
@@ -0,0 +1,90 @@
+function Ballot(a){
+    this.rejectedChoices = [];
+    this.choices = a;
+    this.currIndex = 0;
+    this.currChoice = a[0];
+    this.triggerNextChoice = function(elim){
+        this.rejectedChoices.push(elim);
+        if(this.currChoice==elim){
+            this.nextChoice();
+        }
+    };
+    this.nextChoice = function(){
+        var valid = false;
+        while(!valid){
+            this.currIndex++;
+            if(this.currIndex<=this.choices.length){
+                this.currChoice = a[this.currIndex];
+            }else{
+                this.currChoice = "";
+            }
+            if(!this.rejectedChoices.includes(this.currChoice)){
+                valid = true;
+            }
+        }
+    };
+}
+
+function Race(b,d){
+    this.ballots = b;
+    this.candidates = d;
+    this.results = [];
+    this.tally = function(){
+        var c = [];
+        var e = [];
+        for(var k in this.ballots){
+            if(!e.includes(this.ballots[k].currChoice)){
+                if(this.candidates.includes(this.ballots[k].currChoice)){
+                    e.push(this.ballots[k].currChoice);
+                    c.push([this.ballots[k].currChoice,1]);
+                }
+            }else{
+                for(var o in c){
+                    if(c[o][0]==this.ballots[k].currChoice){
+                        c[o][1]++;
+                    }
+                }
+            }
+        }
+        c.sort(function(a,b){return b[1]-a[1];});
+        return c;
+    };
+    this.elect = function(){
+        var winner = false;
+        while(!winner){
+            this.results.push(this.tally());
+            if(checkWinTally(this.results.slice(-1)[0])){
+                winner = true;
+            }
+            for(var a in this.ballots){
+                this.ballots[a].triggerNextChoice(this.results.slice(-1)[0].slice(-1)[0][0]);
+            }
+        }
+        return this.results;
+    };
+}
+
+function checkWinTally(tally){
+    var s = 0;
+    for(var a in tally){
+        s+=tally[a][1];
+    }
+    return tally[0][1]>=(s/2);
+}
+
+function buildResArray(res){
+    var arr = [];
+    for(var a in res[0]){
+        arr.push([res[0][a][0]]);
+    }
+    for(var a in res){
+        for(var b in arr){
+            for(var c in res[a]){
+                if(res[a][c][0]==arr[b][0]){
+                    arr[b].push(res[a][c][1]);
+                }
+            }
+        }
+    }
+    return arr;
+}
